a90b683bcba211b403bc09de2fa10bcea1a3e48d
maskurArras <dayminoru27@gmail.com>
2022-03-03 05:21:06 +0700

    Autentikasi & Otorisasi halaman login

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// route login
+Route::get('/', fn () => redirect()->route('login'));
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
