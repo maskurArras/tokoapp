763aafeb33f53cb60267a68bddee466e6eb77c24
maskurArras <dayminoru27@gmail.com>
2022-03-04 17:15:52 +0700

    layout template inheritance

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => redirect()->route('login'));
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    return view('home');
 })->name('dashboard');
